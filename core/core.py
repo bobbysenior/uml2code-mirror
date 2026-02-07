b3574778e841cc95ee37d9b1296bc056dd4bc869
thomas <al3ph_0@proton.me>
2026-02-07 11:12:41 +0400

    added python example

---
+++ core/core.py
@@ -0,0 +1,25 @@
+class Langage:
+    Class = None
+    Relations = None
+
+class Class:
+    pass
+
+class Relation:
+    pass
+
+class CppRelations:
+    pass
+
+class CppClass(Class):
+    pass
+
+
+
+class Cpp(Langage):
+    Class = CppClass
+    Relations = CppRelations
+
+choice = input()
+if choice == "CPP":
+    langage = Cpp
